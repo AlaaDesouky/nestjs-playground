23a79cbefdcc0c853b90dbf647c4509bc4b9f80f
Desouky <alaa.desouky33@gmail.com>
2022-06-28 00:48:01 +0200

    Initial setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  register() {
+    return { res: 'register' }
+  }
+
+  login() {
+    return { res: 'login' }
+  }
+}
