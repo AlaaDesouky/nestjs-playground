5b403333f2f39dc75efeb46a7ab988201067ada6
Desouky <alaa.desouky33@gmail.com>
2022-06-28 01:23:22 +0200

    Added global prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, ProductModule, UserModule]
+  imports: [AuthModule, ProductModule, UserModule, PrismaModule, ConfigModule.forRoot()]
 })
 export class AppModule { }
